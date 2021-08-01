5f96b3b98ae8bb800a4a02785385cbcc6775cba0
chris <cseiter@gmail.com>
2021-08-01 16:47:42 -0400

    correct syntax

--- app/application.rb
+++ app/application.rb
@@ -3,7 +3,7 @@ class Application
     @@items = [Item.new("Figs",3.42),Item.new("Pears",0.99)]
 
     def call(env)
-        resp = Rack::response.new
+        resp = Rack::Response.new
         req = Rack::Request.new(env)
 
     end
